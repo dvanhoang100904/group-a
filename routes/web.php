c79ddba959b9f0cb74a8edaa76b0de07078d381d
dvanhoang100904 <daovanhoang100904@gmail.com>
2025-10-16 09:45:25 +0700

    p1. Init list document versions and pagination

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DocumentVersionController;
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+
+Route::get('/documents/{document}/versions', [DocumentVersionController::class, 'index'])
+    ->name('documents.versions.index');
