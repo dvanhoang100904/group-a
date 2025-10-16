934ffafbffe6763db1c5d3fd5e6b7d775c602d6c
ngocdan <132124254+Danngochuynh@users.noreply.github.com>
2025-10-16 09:00:07 +0700

    upload_documents

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UploadController;
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/upload', [UploadController::class, 'index'])->name('upload.index');
+Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
