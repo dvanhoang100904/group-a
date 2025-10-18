d3f8c1cc98db0a71da96873429d75b3a515ebc65
nggialam13 <gialam130704@gmail.com>
2025-10-18 10:45:54 +0700

    bao cao danh sach vi pham

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
     <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
  
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -76,11 +78,6 @@
     {{-- my js --}}
     <script src="{{ asset('assets/js/script.js') }}"></script>
 @stack('scripts')
-
-    {{-- app --}}
-    @vite(['resources/js/app.js'])
-
-    @stack('scripts')
 
 </body>
 
