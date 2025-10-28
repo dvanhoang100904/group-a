e5bf93059d250b2f46f121bb4a4012bb2fabe56f
dvanhoang100904 <daovanhoang100904@gmail.com>
2025-10-28 18:32:52 +0700

    p2 Init api compare document versions

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DocumentVersionApiController;
+use App\Http\Controllers\Api\DocumentVersionCompareController;
 use App\Http\Controllers\Api\UserApiController;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -11,13 +12,14 @@ use App\Http\Controllers\Api\UserApiController;
 
 // Document Versions
 Route::get('/documents/{id}/versions', [DocumentVersionApiController::class, 'index']);
+// Compare
+Route::get('/documents/{documentId}/versions/compare', [DocumentVersionCompareController::class, 'compare']);
 Route::get('/documents/{documentId}/versions/{versionId}', [DocumentVersionApiController::class, 'show']);
 Route::get('/documents/{documentId}/versions/{versionId}/preview', [DocumentVersionApiController::class, 'preview']);
 Route::post('/documents/{id}/versions', [DocumentVersionApiController::class, 'store']);
 Route::get('/documents/{documentId}/versions/{versionId}/download', [DocumentVersionApiController::class, 'download']);
 Route::post('/documents/{documentId}/versions/{versionId}/restore', [DocumentVersionApiController::class, 'restore']);
 Route::delete('/documents/{documentId}/versions/{versionId}', [DocumentVersionApiController::class, 'destroy']);
-
 
 
 // Users
