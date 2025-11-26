522c324603b10fb2c3c8fa263b030f49e2fe9d88
Nam Yen <vonamyen@gmail.com>
2025-11-26 16:21:51 +0700

    fix(sua database folder)

--- database/migrations/2025_10_15_125014_create_folders_table.php
+++ database/migrations/2025_10_15_125014_create_folders_table.php
@@ -13,15 +13,18 @@ return new class extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->increments('folder_id');
-            $table->string('name', 255)->unique();
-            $table->enum('status', ['public', 'private'])->default('private');
+            $table->string('name', 255);
             $table->unsignedInteger('parent_folder_id')->nullable();
             $table->unsignedInteger('user_id');
             $table->timestamps();
 
+            // Indexes
             $table->index(['user_id', 'parent_folder_id'], 'idx_folder_user_parent');
-            $table->index('status', 'idx_folder_status');
             $table->index(['user_id', 'created_at'], 'idx_folder_user_created');
+
+            // Foreign keys (nếu cần)
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('parent_folder_id')->references('folder_id')->on('folders')->onDelete('cascade');
         });
     }
 
